f3d51f52e12f3c37d1f72fd7866cf0eaba0839aa
Martijn <mpkeesmaat@gmail.com>
2020-02-16 20:58:50 +0100

    header

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import gsap from 'gsap';
+
+//simple tween like the old TweenMax.to(...)
+// gsap.to('h1', { duration: 2, x: 100 });
+
+//create a timeline and add a tween
+// var tl = gsap.timeline();
+// tl.to('h1', { duration: 2, x: 100 });
+// tl.to('h1', { duration: 2, y: 100 });
